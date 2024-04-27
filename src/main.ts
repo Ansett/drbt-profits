8bdc4deca5ba8b28aa6508628da31e279ce467c7
Ansett <ansett.san@gmail.com>
2024-04-27 22:03:48 +0200

    Fix - Move Bugsnag API key to .env

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
-import "./assets/base.css";
-import "primeicons/primeicons.css";
-import "primevue/resources/themes/lara-dark-indigo/theme.css";
-import "primeflex/primeflex.css";
+import './assets/base.css'
+import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/lara-dark-indigo/theme.css'
+import 'primeflex/primeflex.css'
 
-import Bugsnag from "@bugsnag/js";
-import BugsnagPluginVue from "@bugsnag/plugin-vue";
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginVue from '@bugsnag/plugin-vue'
 
-import PrimeVue from "primevue/config";
-import ToastService from "primevue/toastservice";
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
 // import DialogService from "primevue/dialogservice";
 
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
 Bugsnag.start({
-  apiKey: "975f4451aae28c61e0ecc6252794bc68",
+  apiKey: import.meta.env.VITE_BUGSNAP_API,
   plugins: [new BugsnagPluginVue()],
-});
-const bugsnagVue = Bugsnag.getPlugin("vue")!;
+})
+const bugsnagVue = Bugsnag.getPlugin('vue')!
 
-const app = createApp(App);
-app.use(bugsnagVue);
-app.use(PrimeVue);
-app.use(ToastService);
+const app = createApp(App)
+app.use(bugsnagVue)
+app.use(PrimeVue)
+app.use(ToastService)
 // app.use(DialogService);
-app.mount("#app");
+app.mount('#app')
